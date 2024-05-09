47a678cf6d33bfe86f37c80c66d9d29335b22c03
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-09 17:07:32 -0300

    Cards estrucutra responsiba y renderizado del contructor productos, asset imagenes

--- src/app/punto1/punto1.component.ts
+++ src/app/punto1/punto1.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-punto1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './punto1.component.html',
   styleUrl: './punto1.component.css'
 })
-export class Punto1Component {
+export class Punto1Component implements OnInit {
+  productos=  [
+    { nombre: 'Auricular Gamer Logitech', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'audicular.webp', precio: 4500.5},
+    { nombre: 'JOYSTICK  PC USB G807', descripcion: 'El Saturn es el joystick cableado ideal, analógicos, vibración y compatibilidad son solo algunas de sus características.', img: 'joystick.png', precio: 2000.99},
+    { nombre: 'Monitor Gamer 22"', descripcion: 'Color : NegroTamaño de panel : 22"Curvo/Plano : PlanoColores : 16.7MTipo de panel : IPSResolución : 1920 x 1080s', img: 'monitor.jpeg', precio: 200.999},
+    { nombre: 'Cooler Cpu Watercooler Gamemax Iceburg 360 Rainbow 295W', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'cooler.jpeg', precio: 4500.5},
+    { nombre: 'Teclado Gamer HyperX Alloy Origins', descripcion: 'Tamaño del enter : GrandeRetroiluminación del teclado : RGB', img: 'teclado.jpeg', precio: 10000.99},
+    { nombre: 'Tableta Gráfica Xp Pen Deco Pro Small', descripcion: 'Sensibilidad a la presion : 8192 nivelesResolucion : 5080 LPITasa de informe : Max 200Rueda mecánica : 1', img: 'tablagrafica.jpeg', precio: 129000.99}
+  ];
 
+ constructor() { }
+
+ ngOnInit() {
+
+ }
 }
