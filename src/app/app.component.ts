5443e03b3921f58d3d0c7c31ebb4ca72653ab495
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-09 16:58:42 -0300

    utilizo el componente punto1 en el app-component principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Punto1Component } from './punto1/punto1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Punto1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
