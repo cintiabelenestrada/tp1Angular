be3c357778dbd2a2c48315f6c29245724501120f
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-14 13:53:26 -0300

    RouterLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Punto1Component } from './punto1/punto1.component';
 import { CarritoComponent } from "./carrito/carrito.component";
 import { Punto2Component } from "./punto2/punto2.component";
@@ -9,7 +9,7 @@ import { Punto2Component } from "./punto2/punto2.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Punto1Component, CarritoComponent, Punto2Component]
+    imports: [RouterOutlet, RouterLink, Punto1Component, CarritoComponent, Punto2Component]
 })
 export class AppComponent {
   title = 'tp1Angular';
