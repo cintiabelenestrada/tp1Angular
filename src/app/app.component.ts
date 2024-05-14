0008e065a3aae09076284b96e84e35ede45b60cc
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-14 13:53:02 -0300

    RouterLink  de las paginas en el header, con espacio para que no se superponga sobre los htmls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Punto1Component } from './punto1/punto1.component';
 import { CarritoComponent } from "./carrito/carrito.component";
+import { Punto2Component } from "./punto2/punto2.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Punto1Component, CarritoComponent]
+    imports: [RouterOutlet, Punto1Component, CarritoComponent, Punto2Component]
 })
 export class AppComponent {
   title = 'tp1Angular';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Punto1Component } from './punto1/punto1.component';
+import { Punto2Component } from './punto2/punto2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'punto1', component: Punto1Component},
+    {path: 'punto2', component: Punto2Component},
+];
