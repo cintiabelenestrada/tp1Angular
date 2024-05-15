0964453afe4478b9bcfae488100cb5cf0edafb2c
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-15 12:54:46 -0300

    Ruteo componente 5punto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Punto1Component } from './punto1/punto1.component';
 import { CarritoComponent } from "./carrito/carrito.component";
 import { Punto2Component } from "./punto2/punto2.component";
+import { Punto5Component } from './punto5/punto5.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterLink, Punto1Component, CarritoComponent, Punto2Component]
+    imports: [RouterOutlet, RouterLink, Punto1Component, CarritoComponent, Punto2Component, Punto5Component]
 })
 export class AppComponent {
   title = 'tp1Angular';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Punto1Component } from './punto1/punto1.component';
 import { Punto2Component } from './punto2/punto2.component';
+import { Punto5Component } from './punto5/punto5.component';
 
 export const routes: Routes = [
     {path: 'punto1', component: Punto1Component},
     {path: 'punto2', component: Punto2Component},
+    {path: 'punto5', component: Punto5Component},
 ];
