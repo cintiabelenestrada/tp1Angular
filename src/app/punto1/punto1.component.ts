15f9c4fc98d3636f2a26220217588806e201f0dd
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-15 12:47:23 -0300

    se intento hacer el calculo para que en el total tome en cuenta la cantidad de producto que se ingrese

--- src/app/punto1/punto1.component.ts
+++ src/app/punto1/punto1.component.ts
@@ -15,7 +15,7 @@ export class Punto1Component implements OnInit {
   productos: Producto[]=  [
     { id: "1", nombre: 'Auricular Gamer Logitech', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'audicular.webp', precio: 4500},
     { id: "2", nombre: 'JOYSTICK  PC USB G807', descripcion: 'El Saturn es el joystick cableado ideal, analógicos, vibración y compatibilidad son solo algunas de sus características.', img: 'joystick.png', precio: 2000},
-    { id: "3", nombre: 'Monitor Gamer 22"', descripcion: 'Color : NegroTamaño de panel : 22"Curvo/Plano : PlanoColores : 16.7MTipo de panel : IPSResolución : 1920 x 1080s', img: 'monitor.jpeg', precio: 200},
+    { id: "3", nombre: 'Monitor Gamer 22"', descripcion: 'Color : NegroTamaño de panel : 22"Curvo/Plano : PlanoColores : 16.7MTipo de panel : IPSResolución : 1920 x 1080s', img: 'monitor.jpeg', precio: 90000},
     { id: "4", nombre: 'Cooler Cpu Watercooler Gamemax Iceburg 360 Rainbow 295W', descripcion: 'Color Negro Modelo G733 Micrófono integrado Tipo de conector inalámbrico', img: 'cooler.jpeg', precio: 4500},
     { id: "5", nombre: 'Teclado Gamer HyperX Alloy Origins', descripcion: 'Tamaño del enter : GrandeRetroiluminación del teclado : RGB', img: 'teclado.jpeg', precio: 10000},
     { id: "6", nombre: 'Tableta Gráfica Xp Pen Deco Pro Small', descripcion: 'Sensibilidad a la presion : 8192 nivelesResolucion : 5080 LPITasa de informe : Max 200Rueda mecánica : 1', img: 'tablagrafica.jpeg', precio: 129000}
@@ -45,9 +45,14 @@ export class Punto1Component implements OnInit {
 
     });
     if (productoExistente) {
+      // si ya se encuentra el producto en el carrito, se incrementa la cantidad, 
+      // y la proxima vez que se haga click en el carrito
+      // no se agrega el producto, sino la cantidad
       productoExistente.cantidad++
     } else{
-     let iCarrito:ProductoCarrito={
+      // si el producto no se encuentra en el array del carrito, 
+      // lo crea de cero como esta definido en ProductoCarrito y lo agrega al array de carrito con el push
+      let iCarrito:ProductoCarrito={
         id: producto.id,
         nombre: producto.nombre,
         img: producto.img,
@@ -63,7 +68,9 @@ export class Punto1Component implements OnInit {
    public calcularTotal(): number {
     let total = 0;
     for (const producto of this.productoCarrito) {
+      // total += producto.precio * producto.cantidad || 0;
       total += producto.precio || 0;
+
     }
     return total;
 }
@@ -73,6 +80,8 @@ public duplicarCarrito(producto: Producto): void {
   const productoExistente = this.productoCarrito.find(prod => prod.id === producto.id);
   if (productoExistente) {
     const productoDuplicado = { ...productoExistente };
+    // el operador de propagación (...) para duplicar el objeto productoExistente en productoDuplicado. Esto creará una copia independiente del objeto existente en lugar de simplemente hacer referencia al mismo objeto.
+    // Después de esta línea, productoDuplicado contendrá una copia exacta de productoExistente, y cualquier modificación que hagas en productoDuplicado no afectará al objeto original productoExistente.
     productoDuplicado.cantidad++;
     this.productoCarrito.push(productoDuplicado);
   } else {
