95077556bc5df70162855a6b6f116f3c3c144b17
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-06-17 18:44:52 -0300

    ruteo del componente alta producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Punto2Component } from "./punto2/punto2.component";
 import { Punto5Component } from './punto5/punto5.component';
 import { ProductoComponent } from './punto1/productosdestacados/components/producto.component';
+import { AltaProductosComponent } from './punto1/altaproductos/components/altaproductos.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterLink, ProductoComponent, Punto2Component, Punto5Component]
+    imports: [RouterOutlet, RouterLink, ProductoComponent, AltaProductosComponent, Punto2Component, Punto5Component]
 })
 export class AppComponent {
-  title = 'tp5Backend';
+  title = 'tp5Frontend';
 }
