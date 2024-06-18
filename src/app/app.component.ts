9c7c4f47c6008bdb7594b7083de61d61e2f9cbe8
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-06-18 03:06:02 -0300

    routing para la alta transacciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Punto5Component } from './punto5/punto5.component';
 import { ProductoComponent } from './punto1/productosdestacados/components/producto.component';
 import { AltaProductosComponent } from './punto1/altaproductos/components/altaproductos.component';
+import { AltatransaccionComponent } from './punto2/altatransaccion/components/altatransaccion.component';
 
 
 @Component({
@@ -10,7 +11,7 @@ import { AltaProductosComponent } from './punto1/altaproductos/components/altapr
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterLink, ProductoComponent, AltaProductosComponent, Punto5Component]
+    imports: [RouterOutlet, RouterLink, ProductoComponent, AltaProductosComponent, AltatransaccionComponent,Punto5Component]
 })
 export class AppComponent {
   title = 'tp5Frontend';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { ProductoComponent } from './punto1/productosdestacados/components/produ
 // import { Punto2Component } from './punto2/punto2.component';
 import { Punto5Component } from './punto5/punto5.component';
 import { AltaProductosComponent } from './punto1/altaproductos/components/altaproductos.component';
+import { AltatransaccionComponent } from './punto2/altatransaccion/components/altatransaccion.component';
 
 export const routes: Routes = [
     {path: 'productosdestacados', component: ProductoComponent},
     {path: 'altaproductos', component: AltaProductosComponent},
-    // {path: 'punto2', component: Punto2Component},
+    {path: 'altatransaccion', component: AltatransaccionComponent},
     {path: 'punto5', component: Punto5Component},
 ];
